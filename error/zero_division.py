f83022be014688124145e1e24f4ab8f7ba97f107
Nortgres <sergtron@ya.ru>
2023-07-27 20:53:21 +0300

    add files

--- error/zero_division.py
+++ error/zero_division.py
@@ -1,9 +1,13 @@
 try:
-    print(5/'a')
-    print(5/0)
+    print(5/1)
+    print(5/5)
 except ZeroDivisionError:
     print('на ноль делить нельзя')
 except TypeError:
     print('Можно передавать только числа')
 except:
     print('Непредвиденная ошибка')
+else:
+    print('Ошибок нет')
+finally:
+    print('Выполняется в любом случае')
