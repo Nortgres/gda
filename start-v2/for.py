326c56b600d6b757e9e8602f3918814e8082fe09
Nortgres <sergtron@ya.ru>
2023-10-26 22:47:47 +0300

    add files

--- start-v2/for.py
+++ start-v2/for.py
@@ -43,9 +43,79 @@ for i in range(text.count('ра')):
     k = text.index('ра', x+1)
     print(k, end=' ')
     x = k
-'''
+
 text = 'Барабанщик бил бой в барабан'
 list1 = list(text)
 for i in list1:
     if i == 'р':
         print(list1{i})
+
+
+list1 = list(input('Введите арифметическое выражения: '))
+a = list1.count(' ')
+k = 0
+for i in range(a):
+    list1.remove(' ')
+
+for i in range(len(list1)):
+    n1 = (i-1)
+    n2 = (i+1)
+    if i ==  '+':
+        k += (int(list1[n1]) + (int(list1[n2])
+    elif i ==  '-':
+
+
+
+##integers = map(int, input('Enter integers: ').split())
+##print(*[pow(val, 2) for index, val in enumerate(integers)])
+
+
+arithmetic_expression = input('Enter arithmetic expression: ')
+list_num = list(map(int, arithmetic_expression.replace('-', ' ').replace('+', ' ').split()))
+list_operations = []
+for i in arithmetic_expression:
+    if i == '-' or i =='+':
+        list_operations.append(i)
+
+result = list_num[0]
+for x in range(len(list_num) - 1):
+    if list_operations[x] == '+':
+        result += list_num[x + 1]
+    else:
+        result -= list_num[x + 1]
+print(result)
+
+
+string = input("Enter string: ")
+iter_str = iter(string)
+while True:
+    symbol = next(iter_str)
+    if symbol != " ":
+        print(symbol, end='')
+    else:
+        break
+isz-faxz-vak
+'''
+list_url = []
+list_url2 = []
+list_url3 = []
+
+while True:
+    url = input('введите url: ')
+    if url != '0':
+        list_url.append(url)
+    else:
+        break
+
+for i in list_url:
+    list_url2.append(i.split(' '))
+
+for i in list_url2:
+    list_temp = []
+    for j in i:
+        if j != '':
+            list_temp.append(j)
+    list_url3.append(list_temp)
+
+for i in list_url3:
+    print('-'.join(i))
